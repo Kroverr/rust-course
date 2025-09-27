2ae41fcdfd28c0b16f30875748866c8e8ccad87c
Krover <opinianojnr04@gmail.com>
2025-09-27 10:14:07 +0800

    feat: experimenting using rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+mod random_number_generator;
+
 fn main() {
-    println!("Hello, world!");
+    // hello_world();
+    // get_input_from_user();
+    // create_mutable_variable();
+    random_number_generator::generate_random_number();
+}
+
+fn get_input_from_user() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+}
+
+fn hello_world() {
+    println!("Hello World!");
+}
+
+fn create_mutable_variable() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
